f34f7a1c5ecc0c05ba8aaed5f50053a798e8bdc1
zbrugman <zach@repiq.com>
2017-04-27 16:01:34 -0500

    markup update

--- src/lib/plugin/ng-bootstrap/tour-step-template.component.ts
+++ src/lib/plugin/ng-bootstrap/tour-step-template.component.ts
@@ -6,21 +6,7 @@ import { Component, TemplateRef, ViewChild, AfterViewInit, ViewEncapsulation } f
   encapsulation: ViewEncapsulation.None,
   selector: 'tour-step-template',
   styles: ['body { max-height: 100vh; }'],
-  template: `
-   <ng-template #tourStep let-step="step" class="repiq-tour">
-    <p class="tour-step-content">{{step?.content}}</p>
-    <div class="row">
-        <div class="tour-step-navigation col-xs-12">
-            <button *ngIf="tourService.hasPrev(step)" class="btn btn-secondary float-xs-left" (click)="tourService.prev()"><i class="fa fa-fw fa-long-arrow-left" aria-hidden="true"></i> Prev</button>
-            <button *ngIf="tourService.hasNext(step)" class="btn  btn-primary float-xs-right" (click)="tourService.next()">Next <i class="fa fa-fw fa-long-arrow-right" aria-hidden="true"></i></button>
-        </div>
-        <div class="col-xs-12">
-            <span class="mt-1 mb-1 small secondary d-block text-xs-center" (click)="tourService.end()">Seen this already? <span class="color-primary cursor-pointer">Do not show this again</span></span>
-        </div>
-    </div>
-</ng-template>
-
-  `,
+  template: `<ng-template #tourStep let-step="step" class="repiq-tour"><p class="tour-step-content">{{step?.content}}</p><div class="row"><div class="tour-step-navigation col-xs-12"><button *ngIf="tourService.hasPrev(step)" class="btn btn-secondary float-xs-left" (click)="tourService.prev()"><i class="fa fa-fw fa-long-arrow-left" aria-hidden="true"></i> Prev</button><button *ngIf="tourService.hasNext(step)" class="btn  btn-primary float-xs-right" (click)="tourService.next()">Next <i class="fa fa-fw fa-long-arrow-right" aria-hidden="true"></i></button></div><div class="col-xs-12"><span class="mt-1 mb-1 small secondary d-block text-xs-center" (click)="tourService.end()">Seen this already? <span class="color-primary cursor-pointer">Do not show this again</span></span></div></div></ng-template>`,
 })
 export class TourStepTemplateComponent implements AfterViewInit {
   @ViewChild('tourStep', { read: TemplateRef }) public tourStepTemplate: TemplateRef<any>;
